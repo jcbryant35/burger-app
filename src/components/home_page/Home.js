babe72689bf96648b09ec795efca064ab9028523
Jason <jcbryant3419@gmail.com>
2021-10-03 01:25:07 -0500

    Initial commits

---
+++ src/components/home_page/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* Components 
+****************************************************/
+import Header from './Header';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+};
+
+export default Home;
+
