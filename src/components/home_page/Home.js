314be4a0fb83f917ef18ada201a1d61bb30a91a1
Jason <jcbryant3419@gmail.com>
2021-10-04 09:33:03 -0500

    Update

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/* Icons
+*************************************************************************/
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 /* Styles
 *****************************************************/
 import './styles/main_home_styles.css';
@@ -17,6 +22,12 @@ function Home() {
                    <Navigation />
                    <Cart />
                 </header>
+                <section id="menuBtnContainer">
+                    <p>Menu</p>
+                    <button id="menuBtn">
+                        <FontAwesomeIcon icon={faChevronDown} id="downArrow" />
+                    </button>
+                </section>
             </main> 
         </div>
     )
