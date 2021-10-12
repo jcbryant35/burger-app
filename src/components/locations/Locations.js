0b2f032407bdf3e155cd8903fc530b4e8fd9df29
Jason <jcbryant3419@gmail.com>
2021-10-12 14:56:37 -0500

    Updates

--- src/components/locations/Locations.js
+++ src/components/locations/Locations.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Map from './Maps';
 
 /* Styles
 **********************************************/
 import './styles/location_styles.css';
+
 
 function Location() {
     return (
@@ -11,6 +13,13 @@ function Location() {
                 <header id="locationHeader">
                     <h1>LOCATIONS</h1>
                 </header>
+                <Map />
+                <article id="locationText">
+                    BOUDREAUX'S BURGERS<br />
+                    117 North Main Street <br />
+                    Jennings, La 70546 <br />
+                    337-259-8432
+                </article>
             </main>
         </div>
     )
---
+++ src/components/locations/Maps.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+
+const markerStyles = {
+    fontSize: '40px',
+    color: '#3D232E'
+}
+
+const pStyles = {
+    backgroundColor: '#D9BCAD',
+    padding: '5px'
+}
+
+
+function Map() {
+  const [viewport, setViewport] = React.useState({
+    latitude: 30.226241,
+    longitude: -92.658002,
+    zoom: 15,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="100%"
+      height="50%"
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+
+    > 
+        <Marker latitude={30.226241} longitude={-92.658002} offsetLeft={-20} offsetTop={-10}>
+            <div>
+                <FontAwesomeIcon icon={faMapMarker} style={markerStyles}/><p style={pStyles}>BOUDREAUX'S BURGERS</p>
+            </div>
+        </Marker>
+    </ReactMapGL>
+  );
+}
+
+export default Map;
